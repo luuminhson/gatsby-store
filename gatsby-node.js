5f6ac211db12d01ac4b6eacabfe586b1cfcd9acf
Son Luu Minh <son.luu@notabasement.com>
2019-08-10 13:13:53 +0700

    Remove unused codes

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 exports.createPages = async ({
   graphql,
-  actions: { createPage, createRedirect }
+  actions: { createPage }
 }) => {
   const pages = await graphql(`
     {
--- src/pages/product-details.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ProductDetails from '../components/ProductDetails/ProductDetails';
-
-const ProductPage = () => <ProductDetails />;
-
-export default ProductPage;
