b7c61da246da1f0f094ac312df3a5eace838a3b1
Son Luu Minh <luuminhson@gmail.com>
2021-02-14 21:38:35 +0700

    Homepage update: Hero section

--- src/templates/index-template.js
+++ src/templates/index-template.js
@@ -20,7 +20,6 @@ type Props = {
 
 const IndexWrapper = styled(`div`)`
   margin: 0 auto;
-  margin-top: calc(-${headerHeight.tablet} - ${spacing['2xl']}px);
   padding-bottom: ${spacing.lg}px;
 
   ${mediaQuery.tabletFrom} {
@@ -41,94 +40,91 @@ const TitleGroup = styled(`div`)`
 // HERO SECTION
 
 const HeroImage = styled(Img)`
-  flex: 1 0 100%;
-  height: 65vh;
+  width: 75%;
+  height: 35vh;
+  margin-left: 25%;
+  border-radius: ${radius.large}px;
 
   ${mediaQuery.tabletFrom} {
-    flex: 1 0 50%;
-    min-height: 500px;
-    height: 50vh;
-    border-radius: ${radius.large}px;
-
-    > div {
-      padding-bottom: 75% !important;
-    }
-  }
-
-  ${mediaQuery.desktop} {
-    > div {
-      padding-bottom: 65% !important;
-    }
+    width: 100%;
+    max-width: 720px;
+    height: auto;
+    max-height: 560px;
+    margin: 0 auto;
   }
 `;
 
+const HeroImageSub = styled(Img)`
+  width: 50%;
+  height: 20vh;
+  margin-top: -${spacing['8xl']}px;
+  margin-right: 55%;
+  border-radius: ${radius.large}px;
+
+  ${mediaQuery.tabletFrom} {
+    width: 30vw;
+    max-width: 560px;
+    height: auto;
+    max-height: 360px;
+    margin-top: -40%;
+    margin-left: 60%;
+    margin-right: auto;
+  }
+
+  ${mediaQuery.desktop} {
+    margin-top: -30%;
+    margin-left: 80%;
+    margin-right: auto;
+  }
+`;
+
+const MaskText = styled('span')`
+  color: transparent;
+  overflow: hidden;
+  background: url('${props => props.maskSrc}') center center;
+  background-size: cover;
+  -webkit-text-fill-color: transparent;
+  -webkit-background-clip: text;
+  -moz-background-clip: text;
+  -o-background-clip: text;
+  background-clip: text;
+`;
+
 const HeroTitle = styled(FontStyle.h1)`
-  font-size: 1.6rem;
-  line-height: 2.4rem;
+  font-size: 3.6rem;
+  line-height: 4.4rem;
   margin-bottom: ${spacing.lg}px;
 
   ${mediaQuery.tabletFrom} {
-    font-size: 1.6rem;
-    line-height: 2.4rem;
+    font-size: 2.4rem;
+    line-height: 3rem;
     max-width: 68vw;
     margin-bottom: ${spacing.xl}px;
   }
 
   ${mediaQuery.desktop} {
-    font-size: 2.4rem;
-    line-height: 3.2rem;
-    margin-bottom: ${spacing.xl}px;
+    font-size: 4rem;
+    line-height: 4.85rem;
+  }
+
+  ${mediaQuery.desktopLarge} {
+    font-size: 6rem;
+    line-height: 6.75rem;
   }
 `;
 
 const HeroSection = styled(`section`)`
   position: relative;  
-  display: flex;
-  align-items: center;
-  margin-bottom: 15%;
-
-  &:after {
-    content: '';
-    display: block;
-    position: absolute;
-    left: 0;
-    top: 0;
-    width: 100%;
-    height: 100%;
-    background-color: rgba(0,0,0,0.4);
-    z-index: 1;
-  }
+  display: grid;
+  grid-template-rows: auto;
+  row-gap: ${spacing.xl}px;
+  padding: 0 ${spacing.lg}px;
+  margin: 5% 0 15%;
 
   ${mediaQuery.tabletFrom} {
-    margin-top: 5%;
-
-    &:after {
-      display: none;
-    }
-  }
-
-  ${TitleGroup} {
-    position: absolute;
-    bottom: ${spacing.xl}px;
-    // transform: translate3d(0, -20%, 0);
-    // text-align: center;
-    padding: 0 ${spacing.lg}px;
-    z-index: 10;
-
-    ${mediaQuery.tabletFrom} {
-      position: static;
-      flex: 1 0 50%;
-      text-align: left;
-      padding: 0 5% 0 8%; 
-    }
-
-    ${HeroTitle} {
-      color: ${colors.white};
-
-      ${mediaQuery.tabletFrom} {
-        color: inherit;
-      }
-    }
+    grid-template-columns: 40% 55%;
+    column-gap: 5%;
+    padding: 0;
   }
 `;
 
@@ -233,6 +229,8 @@ class IndexTemplate extends React.Component<Props> {
     const blogPost = data.blogStrip.edges;
 
     const heroImageSrc = data.heroImage.childImageSharp.fluid;
+    const heroImageSubSrc = data.heroImageSub.childImageSharp.fluid;
+    const heroTitleMaskSrc = data.heroTitleMaskImage.childImageSharp.fluid.src;
 
     const latestProductList = data.latestProducts.edges;
 
@@ -241,11 +239,12 @@ class IndexTemplate extends React.Component<Props> {
         <IndexWrapper>
           <ScrollAnimation>
             <HeroSection>
-              <HeroImage fluid={heroImageSrc} />
               <TitleGroup>
-                <HeroTitle>Happiness is homemade</HeroTitle>
+                <HeroTitle>Happiness is <MaskText maskSrc={heroTitleMaskSrc}>homemade</MaskText></HeroTitle>
                 <PrimaryButton to='/products'>Mua bánh về ăn</PrimaryButton>
               </TitleGroup>
+              <HeroImage fluid={heroImageSrc} />
+              <HeroImageSub fluid={heroImageSubSrc} />
             </HeroSection>
           </ScrollAnimation>
 
@@ -284,12 +283,12 @@ export default props => (
       setToIndexPage,
       viewportIs
     }) => (
-        <IndexTemplate
-          {...props}
-          setPage={setToIndexPage}
-          viewportIs={viewportIs}
-        />
-      )}
+      <IndexTemplate
+        {...props}
+        setPage={setToIndexPage}
+        viewportIs={viewportIs}
+      />
+    )}
   </InterfaceContext.Consumer>
 )
 
@@ -337,6 +336,20 @@ export const query = graphql`
       childImageSharp {
         fluid(maxWidth: 1000, quality: 100) {
           ...GatsbyImageSharpFluid
+        }
+      }
+    }
+    heroImageSub: file(relativePath: { eq: "index-demo-sub.jpg" }) {
+      childImageSharp {
+        fluid(maxWidth: 1000, quality: 100) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+    heroTitleMaskImage: file(relativePath: { eq: "hero-title-mask.png" }) {
+      childImageSharp {
+        fluid(maxWidth: 1000, quality: 100) {
+          ...GatsbyImageSharpFluid_noBase64
         }
       }
     }
